74609695797b1cb9bdb16132185d4d0b57b540e9
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-04-12 11:17:24 +0530

    12-4-24

---
+++ index2.js
@@ -0,0 +1,117 @@
+// q1--------------------------
+// /let inc=+prompt("enter your number")
+
+// if (inc>0&&inc<=10000) {
+//       let income=inc*5/100;
+//       console.log("text maunt=",income);
+// } else if(inc>10001&&inc<=20000){
+//     let income=inc*10/100;
+//       console.log("text maunt=",income);
+// }else if(inc>20001&&inc<=30000){
+//     let income=inc*15/100;
+//       console.log("text maunt=",income);
+// }else if(inc>=30000){
+//     let income=inc*20/100;
+//       console.log("text maunt=",income);
+// }
+
+
+
+
+// q2---------------------------
+
+// let num=+prompt("enter your number")
+
+
+
+// switch (num) {
+//     case 1:
+// console.log("jan");
+//         break;
+//     case 2:
+//         console.log("feb");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+//     case 7:
+//         console.log("juley");
+//         break;
+//     case 8:
+//         console.log("aug");
+//         break;
+//     case 9:
+//         console.log("sep");
+//         break;
+//     case 10:
+//         console.log("oct");
+//         break;
+//     case 11:
+//         console.log("nov");
+//         break;
+//     case 12:
+//         console.log("dec");
+//         break;
+//     default:
+//         console.log("invelid number");
+//         break;
+// }
+
+
+
+// q3----------------------------------------
+
+
+// let prode=+prompt("enter your prodecut")
+
+// if (prode>0&&prode<=100) {
+//       let price=prode*5/100;
+//       let total=prode-price;
+//       console.log(" maunt=", total);
+// }
+//  else if(prode>101&&prode<=500){
+//     let price=prode*10/100;
+//       let total=prode-price;
+//       console.log(" maunt=", total);
+// }else if(prode>500){
+//     let price=prode*15/100;
+//       let total=prode-price;
+//       console.log(" maunt=", total);
+// }
+
+
+
+
+// q4-----------------------------
+
+
+
+
+
+// let weight=+prompt("enter your weight")
+// let height=+prompt("enter your height in minter")
+
+// let weight=95;
+// let height=1.88;
+
+// let bmi=weight/(height*height);
+
+// if (bmi<18.5) {
+//       console.log("underweight=",bmi);
+// }
+//  else if(bmi>18.5>1001&&bmi<24.9){
+//       console.log("normal=",bmi);
+// }else if(bmi>25>1001&&bmi<29.9){
+//       console.log("overweight=",bmi);
+// }else if(bmi>30){
+//     console.log("obese=",bmi);
+// }
